b5802e979c2830e8e4b7ccde15dae05f37fac9f9
unknown <oezys14@gmail.com>
2024-01-22 19:21:18 -0800

    New File

---
+++ form_1.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        welcome<?php echo $_POST["firstname"] , $_POST["lastname"]; ?><br>\
+        Your email address is: <?php echo $_POST["email"]; ?><br>
+        Your address is : <?php echo $_POST["address"]; ?><br>
+</body>
+</html>
