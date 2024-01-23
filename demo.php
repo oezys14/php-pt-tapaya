0dfd0d772ca062431fc7d97accacc8f3e26bd570
unknown <oezys14@gmail.com>
2024-01-22 19:07:05 -0800

    New file

---
+++ demo.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Simple Form Processing</title>
+</head>
+
+<body>
+    <form action="form_1.php" method="post">
+        FirstName:
+        <input type="text" name="FirstName" required/>
+        <br>
+        <br>
+        LastNAme
+        <input type="text" name="LastNAme" required/>
+        <br>
+        <br>
+        Address 
+        <input type="text" name="Address" required/>
+        <br>
+        <br>
+        Email Address: 
+        <input type="text" name="Email Address" required/>
+        <br>
+        <br>
+        Password:
+        <input type="text" name="Password" required/>
+        <br>
+        <br>
+        <input type="submit" value="submit">
+    </form>
+</body>
+</html>
